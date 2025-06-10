848ee68cbb0e73f860dd21760577d4b484eefe0a
Milena Carvalho <milena200100@edu.unipar.br>
2025-06-10 16:02:41 -0300

    Update script.js

--- trabalho2b/script.js
+++ trabalho2b/script.js
@@ -49,4 +49,82 @@ botao3.onclick = function (){
     calcular3();
 }
 
+//questao 5
 
+let saldo = document.querySelector("#saldo");
+let botao5 = document.querySelector("#botao5");
+let resultado5 = document.querySelector("#resultado5");
+
+function calcular5() {
+    let s5 = Number(saldo.value); 
+
+  
+    if (isNaN(s5) || s5 < 0) {
+        saldo.classList.add('invalid');  
+        resultado5.textContent = '';  
+        return alert('Invalid');
+    }
+
+    saldo.classList.remove('invalid');
+
+    
+    let percentual = s5 <= 200 ? 0 : s5 <= 400 ? 0.2 : s5 <= 600 ? 0.3 : 0.4;
+    let valorCredito = s5 * percentual;
+
+    
+    resultado5.innerHTML = `Saldo M&eacute;dio: R$ ${s5.toFixed(2)} - ${valorCredito > 0 ? `Cr&eacute;dito concedido: R$ ${valorCredito.toFixed(2)}` : 'Nenhum cr&eacute;dito dispon&iacute;vel.'}`;
+}
+
+
+botao5.onclick = function () {
+    calcular5();
+}
+
+//questoes 7  
+let preco = document.querySelector("#preco");
+let pagamento = document.querySelector("#pagamento");
+let botao7 = document.querySelector("#botao7");
+let resultado7 = document.querySelector("#resultado7");
+
+function calcular7 (){
+    let p7 = Number(preco.value);
+    if (isNaN(p7)|| p7 <= 0) {
+        alert("Por favor, insira um valor!");
+        return;
+    }
+    let condpagamento = pagamento.value;
+    let totalpago = 0; 
+    let mensagem = "";
+
+    switch(condpagamento) {
+        case 'a': 
+            let desconto10 = p7 * (10/100);
+            totalpago = p7 - desconto10;
+            mensagem = `&agrave; vista no dinheiro ou cheque: Desconto 10%.
+             O total a pagar &eacute; R$ ${totalpago.toFixed(2)}.`;
+             break;
+        case 'b':
+            let desconto15 = p7 * (15/100); 
+            totalPago = p7 - desconto15;
+            mensagem = `À vista no cart&atilde;o de cr&eacute;dito: Desconto de 15%. O total a pagar é R$ ${totalPago.toFixed(2)}.`;
+            break;
+        case 'c':
+            totalPago = p7;
+            mensagem = `Em duas vezes, sem juros. O total a pagar &eacute; R$ ${totalPago.toFixed(2)}.`;
+            break;
+        case 'd':
+            let juros10 = p7 * (10/100); 
+            totalPago = p7 + juros10; 
+            mensagem = `Em duas vezes, com juros de 10%. O total a pagar é R$ ${totalPago.toFixed(2)}.`;
+            break;
+        default:
+            mensagem = "Opção inválida.";
+            break;
+    }
+
+    resultado7.innerHTML = mensagem;
+}  
+botao7.onclick = function (){
+    calcular7();
+
+}  
